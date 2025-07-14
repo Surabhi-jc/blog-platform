64569132f0f1d23f17e38eb8941cf40a0dbbf886
Surabhi <surabhi.sonam@jarvis.consulting>
2025-07-14 10:07:31 +0530

    Initial commit + signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+protect_from_forgery with: :null_session
+
+    def create
+        user= User.new(user_params)
+
+        if(user.save)
+            render json: {message: "User creation is successful", user: user}, status: :created
+        else
+            render json: {errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+
+end
