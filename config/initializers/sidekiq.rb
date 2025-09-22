44256c7f8a19c0f17008ebf53c39045401b46815
Surabhi <surabhi.sonam@jarvis.consulting>
2025-09-22 15:04:20 +0530

    redis config to work on render

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 require 'sidekiq'
 require 'sidekiq/cron/job'
 
-redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')
+# redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')
+
+redis_url = ENV['REDIS_URL'] || 'redis://redis:6379/0'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url }
